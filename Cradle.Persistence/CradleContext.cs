aefe82944501dad74e51b24e0257392c8be4760e
Praise Balogun <pbalogun@axxess.com>
2024-01-18 17:04:25 +0100

    added infrastructure

---
+++ Cradle.Persistence/CradleContext.cs
@@ -0,0 +1,16 @@
+﻿using Cradle.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cradle.Persistence
+{
+    public class CradleContext(DbContextOptions<CradleContext> options) : DbContext(options)
+    {
+        public DbSet<Course> Courses { get; set; }
+    }
+  
+}
